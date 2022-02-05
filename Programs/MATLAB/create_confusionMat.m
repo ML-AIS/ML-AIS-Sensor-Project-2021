ff66eef345746e6ccde9b1e1a0a160502ad75e60
paweenp <paween.pongsomboon@gmail.com>
2022-02-05 15:51:22 +0100

    update create_confusion matrix  program and add more evaluation results

--- Programs/MATLAB/create_confusionMat.m
+++ Programs/MATLAB/create_confusionMat.m
@@ -13,7 +13,7 @@ TP = 0; FP = 0; TN = 0; FN = 0;
 num_fileswithheaders = 0;
 num_fileswithoutheaders = 0;
 %% ---- Case for Confusion Matrix -----
-% | TN | FP |
+% | TP | FP |
 % | -- | -- |
 % | FN | TN |
 %
@@ -23,157 +23,205 @@ num_fileswithoutheaders = 0;
 %       FN : Empty --> Not Empty : Overestimation
 %       TN : Not Empty --> Not Empty
 
-%%
+%% list path and prepare array for storing evaluation result
+
+% Data from 2022.01.20
+data_path = "C:\workspace\FRA-UAS\semester3\ML-AIS\ML-AIS-Sensor-Project-2021\Data\Sensor-1_multiple_threshold\";
+
+% Data from 2022.02.04
+% data_path = "C:\workspace\FRA-UAS\semester3\ML-AIS\ML-AIS-Sensor-Project-2021\Data\2022.02.04\threshold_setting\";
+
+cm_folder = "confusion_matrix 2022.01.20\";
+cm_filetype = '.png';
+
+eval_filename = "Evaluation Result 2022.01.20.xlsx";
+
+list_folder = dir(data_path);
+% Remove current and prev dir
+list_folder = list_folder(3:end, :);
+
+tbl_var = { ...
+    'ThresholdSetting', ...
+    'Amount of Data', ...
+    'Accuracy', ...
+    'Precision', ...
+    'Recall', ...
+    'F1-Score'
+    };
+
+tbl_var_type  = { ...
+    'string', ...
+    'double', ...
+    'double', ...
+    'double', ...
+    'double', ...
+    'double'
+    };
+
+col = size(tbl_var, 2);
+row = size(list_folder, 1);
+tbl_size = [row col];
+
+% row_names = strings(row, 1);
+% row_names(1:end) = list_folder(1:end).name;
+
+% Create table for evaluation result
+
+eval_tbl = table( ...
+    'Size', tbl_size, ...
+    'VariableNames', tbl_var, ...
+    'VariableTypes', tbl_var_type ...
+    );
+
+
 % Keyword for search
-% path of normal files
-% path = "C:/workspace/FRA-UAS/semester3/ML-AIS/ML-AIS-Sensor-Project-2021/Data/Sensor-1/";
-% path = "C:\workspace\FRA-UAS\semester3\ML-AIS\ML-AIS-Sensor-Project-2021\Data\Sensor-1_multiple_threshold/160,12000/";
-% path = "C:\workspace\FRA-UAS\semester3\ML-AIS\ML-AIS-Sensor-Project-2021\Data\Sensor-1_multiple_threshold/160,14000/";
-% path = "C:\workspace\FRA-UAS\semester3\ML-AIS\ML-AIS-Sensor-Project-2021\Data\Sensor-1_multiple_threshold/160,16000/";
-% path = "C:\workspace\FRA-UAS\semester3\ML-AIS\ML-AIS-Sensor-Project-2021\Data\Sensor-1_multiple_threshold/160,20000/";
-% path = "C:\workspace\FRA-UAS\semester3\ML-AIS\ML-AIS-Sensor-Project-2021\Data\Sensor-1_multiple_threshold/180,10000/";
-% path = "C:\workspace\FRA-UAS\semester3\ML-AIS\ML-AIS-Sensor-Project-2021\Data\Sensor-1_multiple_threshold/180,12000/";
-% path = "C:\workspace\FRA-UAS\semester3\ML-AIS\ML-AIS-Sensor-Project-2021\Data\Sensor-1_multiple_threshold/180,14000/";
-% path = "C:\workspace\FRA-UAS\semester3\ML-AIS\ML-AIS-Sensor-Project-2021\Data\Sensor-1_multiple_threshold/200,10000/";
-% path = "C:\workspace\FRA-UAS\semester3\ML-AIS\ML-AIS-Sensor-Project-2021\Data\Sensor-1_multiple_threshold/200,12000/";
-% path = "C:\workspace\FRA-UAS\semester3\ML-AIS\ML-AIS-Sensor-Project-2021\Data\Sensor-1_multiple_threshold/200,14000/";
-% path = "C:\workspace\FRA-UAS\semester3\ML-AIS\ML-AIS-Sensor-Project-2021\Data\Sensor-1_multiple_threshold/250,15000/";
-
-
-% path of processed files with deleting first 10 rows of data
-% path = "C:/workspace/FRA-UAS/semester3/ML-AIS/ML-AIS-Sensor-Project-2021/Data/Sensor-1/proc/";
-% path = "C:\workspace\FRA-UAS\semester3\ML-AIS\ML-AIS-Sensor-Project-2021\Data\Sensor-1_multiple_threshold_proc/";
-% path = "C:\workspace\FRA-UAS\semester3\ML-AIS\ML-AIS-Sensor-Project-2021\Data\Sensor-1_multiple_threshold_proc/160,12000/";
-% path = "C:\workspace\FRA-UAS\semester3\ML-AIS\ML-AIS-Sensor-Project-2021\Data\Sensor-1_multiple_threshold_proc/160,14000/";
-% path = "C:\workspace\FRA-UAS\semester3\ML-AIS\ML-AIS-Sensor-Project-2021\Data\Sensor-1_multiple_threshold_proc/160,16000/";
-% path = "C:\workspace\FRA-UAS\semester3\ML-AIS\ML-AIS-Sensor-Project-2021\Data\Sensor-1_multiple_threshold_proc/160,20000/";
-% path = "C:\workspace\FRA-UAS\semester3\ML-AIS\ML-AIS-Sensor-Project-2021\Data\Sensor-1_multiple_threshold_proc/180,10000/";
-path = "C:\workspace\FRA-UAxS\semester3\ML-AIS\ML-AIS-Sensor-Project-2021\Data\Sensor-1_multiple_threshold_proc/180,12000/";
-% path = "C:\workspace\FRA-UAS\semester3\ML-AIS\ML-AIS-Sensor-Project-2021\Data\Sensor-1_multiple_threshold_proc/180,14000/";
-% path = "C:\workspace\FRA-UAS\semester3\ML-AIS\ML-AIS-Sensor-Project-2021\Data\Sensor-1_multiple_threshold_proc/200,10000/";
-% path = "C:\workspace\FRA-UAS\semester3\ML-AIS\ML-AIS-Sensor-Project-2021\Data\Sensor-1_multiple_threshold_proc/200,12000/";
-% path = "C:\workspace\FRA-UAS\semester3\ML-AIS\ML-AIS-Sensor-Project-2021\Data\Sensor-1_multiple_threshold_proc/200,14000/";
-% path = "C:\workspace\FRA-UAS\semester3\ML-AIS\ML-AIS-Sensor-Project-2021\Data\Sensor-1_multiple_threshold_proc/250,15000/";
-
-
-
 keyword = "**/FFT_*.txt"; % change this keyword to filter files according to case.
 
+disp("Program create confusion matrix beinn! ...");
 
-path_with_key = path+keyword;
-% 1. List file paths with keyword
-listdir = dir(path_with_key);
-
-% Declare array to contain actual result and classified results 
-result_actual = strings(size(listdir, 1), 1);
-result_classified = strings(size(result_actual, 1), 1);
-counter = 1;
-
-for index = 1:size(listdir)
-    fullfilepath = fullfile(listdir(index).folder, listdir(index).name);
+for k = 1:tbl_size(1)
     
-    % 2. Check if file has headers field
-    content = readmatrix(fullfilepath);
-    
-    % guard for empty file
-    if isempty(content)
-        num_fileswithoutheaders = num_fileswithoutheaders + 1;
-        continue;
-    end
+    path_fullfolder = fullfile(list_folder(k).folder, list_folder(k).name);
+    path_withkey = fullfile(path_fullfolder,keyword);
+    % 1. List file paths with keyword
+    listdir = dir(path_withkey);
     
     
-    checkones = all(content(:,1) == 64);
+    % Declare array to contain actual result and classified results
+    result_actual = strings(size(listdir, 1), 1);
+    result_classified = strings(size(result_actual, 1), 1);
+    counter = 1;
     
-    % headers have 64 (bit length) in the first column
-    if (checkones == true) % headers exist
+    for index = 1:size(listdir)
+        fullfilepath = fullfile(listdir(index).folder, listdir(index).name);
         
-        num_fileswithheaders = num_fileswithheaders + 1;
+        % 2. Check if file has headers field
+        content = readmatrix(fullfilepath);
         
-        % --------------------------------------
-        % check classification result (column 3)
-        % 1: object
-        % 2: Human
-        % --------------------------------------
-        for jndex = 1:size(content, 1)
-            classification = content(jndex, 3);
+        % guard for empty file
+        if isempty(content)
+            num_fileswithoutheaders = num_fileswithoutheaders + 1;
+            continue;
+        end
+        
+        
+        checkones = all(content(:,1) == 64);
+        
+        % headers have 64 (bit length) in the first column
+        if (checkones == true) % headers exist
             
+            num_fileswithheaders = num_fileswithheaders + 1;
             
-            % check keyword
-            % 1. Empty "Empty Seat"
-            if contains(fullfilepath, "FFT_E")
+            % --------------------------------------
+            % check classification result (column 3)
+            % 1: object
+            % 2: Human
+            % --------------------------------------
+            for jndex = 1:size(content, 1)
+                classification = content(jndex, 3);
                 
-                result_actual(counter) = "Empty Seat";
                 
-                % Classification 
-                % 1 : Empty
-                % 2 : Human
-                switch classification
-                    case 1
-                        TP = TP + 1;
-                        result_classified(counter) = "Empty Seat";
-                    case 2
-                        FN = FN + 1; % Empty -> Not Empty
-                        result_classified(counter) = "Human";
-                    otherwise
-                        disp("Problem with Switch Case E condition");
+                % check keyword
+                % 1. Empty "Empty Seat"
+                if contains(fullfilepath, "FFT_E")
+                    
+                    result_actual(counter) = "Empty Seat";
+                    
+                    % Classification
+                    % 1 : Empty
+                    % 2 : Human
+                    switch classification
+                        case 1
+                            result_classified(counter) = "Empty Seat";
+                        case 2
+                            % Empty -> Not Empty
+                            result_classified(counter) = "Human";
+                        otherwise
+                            disp("Problem with Switch Case E condition");
+                    end
+                    
+                    counter = counter + 1;
+                    
+                elseif contains(fullfilepath, "FFT_H")
+                    
+                    result_actual(counter) = "Human";
+                    
+                    % Classification
+                    % 1 : Empty
+                    % 2 : Human
+                    switch classification
+                        case 1
+                            % Not Empty -> Empty FP
+                            result_classified(counter) = "Empty Seat";
+                        case 2
+                            result_classified(counter) = "Human";
+                        otherwise
+                            disp("Problem with Switch Case H condition");
+                    end
+                    
+                    counter = counter + 1;
+                    
+                else
+                    disp("This is a problem !!!");
                 end
                 
-                counter = counter + 1;
-                
-            elseif contains(fullfilepath, "FFT_H")
-                
-                result_actual(counter) = "Human";
-                
-                % Classification 
-                % 1 : Empty
-                % 2 : Human
-                switch classification
-                    case 1
-                        FP = FP + 1; % Not Empty -> Empty FP
-                        result_classified(counter) = "Empty Seat";
-                    case 2
-                        TN = TN + 1;
-                        result_classified(counter) = "Human";
-                    otherwise
-                        disp("Problem with Switch Case H condition");
-                end
-                
-                counter = counter + 1;
-                
-            else
-                disp("This is a problem !!!");
             end
+            
+        else % headers not exist
+            num_fileswithoutheaders = num_fileswithoutheaders + 1;
             
         end
         
-    else % headers not exist
-        num_fileswithoutheaders = num_fileswithoutheaders + 1;
-        
     end
+    
+    
+    % Create Confusion Matrix
+    % 1. Change array type from string array to categorical
+    % 2. plot confusion Matrix
+    % 3. Generate Images for confusion matrix
+    result_actual = categorical(result_actual);
+    result_classified = categorical(result_classified);
+    
+    plotconfusion(result_classified, result_actual);
+    cm = confusionmat(result_classified, result_actual);
+    
+    % Confusion Matrix 
+    % TP | FN
+    % -------
+    % FP | TN
+    
+    TP = cm(1); FN = cm(3);
+    FP = cm(2); TN = cm(4);
+    
+    if ~exist(cm_folder, 'dir')
+        mkdir(cm_folder)
+    end    
+    
+    save_cm_path = fullfile( cm_folder, strcat(list_folder(k).name, cm_filetype) );
+    saveas(gcf, save_cm_path);
+    
+    accuracy = sum(result_classified == result_actual)/numel(result_actual);
+    precision = TP/(TP+FP);
+    recall= TP/(TP+FN);
+    F_score=2*recall*precision/(precision+recall);
+    
+    % Evaluation
+    eval_tbl.ThresholdSetting(k) = list_folder(k).name;
+    eval_tbl.("Amount of Data")(k) = counter;
+    eval_tbl.Accuracy(k) = accuracy;
+    eval_tbl.Precision(k) = precision;
+    eval_tbl.Recall(k) = recall;
+    eval_tbl.("F1-Score")(k) = F_score;
+    
+    
+    disp(["Finished process on ",  list_folder(k).name]);
     
 end
 
+writetable(eval_tbl, eval_filename);
+disp(["Finised writing result file: ", eval_filename]);
 
-% Create Confusion Matrix
-% 1. Change array type from string array to categorical
-% 2. plot confusion Matrix
-result_actual = categorical(result_actual);
-result_classified = categorical(result_classified);
-
-plotconfusion(result_classified, result_actual);
-
-% Evaluation
-accuracy = sum(result_classified == result_actual)/numel(result_actual);
-precision = TP/(TP+FP);
-recall= TP/(TP+FN);
-F_score=2*recall*precision/(precision+recall);
-
-disp("accuracy : " + accuracy);
-disp("precision : " + precision);
-disp("recall : " + recall);
-disp("F_score : " + F_score);
-
-
+disp("End of Program!");
 toc
 
 
